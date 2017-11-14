afe32de1129b68e7f5feb454bb32dad89bb6deb6
Gary Mazzone Peters <gary.m.peters@vanderbilt.edu>
2017-11-13 19:33:52 -0800

    Half way through adding in new things

---
+++ src/web/movies/app.js
@@ -0,0 +1,36 @@
+$(document).ready(function () {
+
+    getMoview();
+
+    function getMoview() {
+        console.log("hit get movie",finalvalue)
+
+        jQuery.ajax ({
+            // url:  "/api/contributors/" + finalvalue.contributorId + "/movies?offset=" + offset + "&count="  + count,
+            url:"/api/movies/"
+            type: "GET",
+            beforeSend: function (xhr) {
+                xhr.setRequestHeader ("Authorization", token);
+            }
+        })
+            .done(function(data){
+                total = data.metadata.total;
+                console.log(total);
+                $("#page").text("Page " + Math.floor(offset/count+1) + " of " + (Math.ceil(total/count)));
+                $("#movieTable").find(".cloned").remove();
+                data.content.forEach(function(item){
+                    $( "#movieRow" ).clone().prop("id",item.id).appendTo( "#movieTable" );
+                    $("#"+item.id).find("#mName").text(item.name);
+                    $("#"+item.id).find("#genre").text(item.genre);
+                    $("#"+item.id).find("#level").text(item.level);
+                    $("#"+item.id).prop("class","cloned");
+                    $("#"+item.id).show();
+                });
+            })
+            .fail(function(data){
+                $("#movieList").text("Sorry no cars");
+            })
+
+    }
+
+});
