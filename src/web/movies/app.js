3a446cc590e8869ca38969559b49852cc9b05817
Gary Mazzone Peters <gary.m.peters@vanderbilt.edu>
2017-11-13 21:55:35 -0800

    FINISHED HAHA CC in your FACE!!!! WOOOOO

--- src/web/movies/app.js
+++ src/web/movies/app.js
@@ -1,22 +1,23 @@
+var offset = 0;
+var count = 20;
+var total = -1;
 $(document).ready(function () {
+    console.log( "go!" );
 
     getMoview();
 
     function getMoview() {
-        console.log("hit get movie",finalvalue)
+        //console.log("hit get movie",finalvalue)
 
         jQuery.ajax ({
             // url:  "/api/contributors/" + finalvalue.contributorId + "/movies?offset=" + offset + "&count="  + count,
-            url:"/api/movies/"
-            type: "GET",
-            beforeSend: function (xhr) {
-                xhr.setRequestHeader ("Authorization", token);
-            }
+            url: "/api/movies/",
+            type: "GET"
         })
             .done(function(data){
-                total = data.metadata.total;
-                console.log(total);
-                $("#page").text("Page " + Math.floor(offset/count+1) + " of " + (Math.ceil(total/count)));
+                //total = data.metadata.total;
+                //console.log(total);
+                //$("#page").text("Page " + Math.floor(offset/count+1) + " of " + (Math.ceil(total/count)));
                 $("#movieTable").find(".cloned").remove();
                 data.content.forEach(function(item){
                     $( "#movieRow" ).clone().prop("id",item.id).appendTo( "#movieTable" );
